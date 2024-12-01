37c35189248cea8916aed0ce95feb00ba91f835b
icr0z <84248928+icr0z@users.noreply.github.com>
2024-12-01 18:59:50 +0100

    Update guildCreate.js

--- events/client/guildCreate.js
+++ events/client/guildCreate.js
@@ -16,10 +16,10 @@ module.exports = async (client, guild) => {
 
 	} else if (db.get(`antijoinbot_${client.user.id}`) === true) {
 		let own = db.all().filter(data => data.ID.startsWith(`ownermd_${client.user.id}`)).sort((a, b) => b.data - a.data)
-		client.config.owner.forEach(u => client.users.cache.get(u).send(`Je viens de rejoindre ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>) mais j'ai quitter car l'antijoin du bot est activé (\`botconfig\`)`))
+		client.config.owner.forEach(u => client.users.cache.get(u).send(`Je viens de rejoindre ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>) mais j'ai quitté car l'**AntiJoin** du bot est activé (\`botconfig\`)`))
 
 		own.filter(x => client.users.cache.get(x.ID.split('_')[2])).map((m, i) => {
-			client.users.cache.get(m.ID.split('_')[2]).send(`Je viens de rejoindre ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>) mais j'ai quitter car l'antijoin du bot est activé (\`botconfig\`)`)
+			client.users.cache.get(m.ID.split('_')[2]).send(`Je viens de rejoindre ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>) mais j'ai quitté car l'**AntiJoin** du bot est activé (\`botconfig\`)`)
 		})
 		return guild.leave()
 	}
